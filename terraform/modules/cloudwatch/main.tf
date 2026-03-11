0be52bdd85f6261fe65ba87229c17107ec363b9b
Luekrit Kongkamon <luekrit.k@gmail.com>
2026-03-11 23:22:42 +1100

    update cloudwatch module

--- terraform/modules/cloudwatch/main.tf
+++ terraform/modules/cloudwatch/main.tf
@@ -1,31 +1,34 @@
-resource "aws_cloudwatch_event_rule" "security_detection" {
-  name        = "${var.project_name}-${var.environment}-iam-detection"
-  description = "Detect IAM user creation events"
+resource "aws_cloudwatch_event_rule" "security_detection_rule" {
+  name        = "${var.project_name}-${var.environment}-iam-policy-change"
+  description = "Detect IAM policy changes"
 
   event_pattern = jsonencode({
-    source = ["aws.iam"]
-
-    detail-type = ["AWS API Call via CloudTrail"]
-
-    detail = {
-      eventSource = ["iam.amazonaws.com"]
-      eventName   = ["CreateUser"]
+    "source": ["aws.iam"],
+    "detail-type": ["AWS API Call via CloudTrail"],
+    "detail": {
+      "eventSource": ["iam.amazonaws.com"],
+      "eventName": [
+        "CreatePolicy",
+        "DeletePolicy",
+        "AttachRolePolicy",
+        "PutRolePolicy"
+      ]
     }
   })
 
   tags = var.tags
 }
 
-resource "aws_cloudwatch_event_target" "lambda_trigger" {
-  rule = aws_cloudwatch_event_rule.security_detection.name
-  arn  = var.lambda_function_arn
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.security_detection_rule.name
+  target_id = "LambdaSecurityRemediation"
+  arn       = var.lambda_function_arn
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
+  statement_id  = "AllowEventBridgeInvoke"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_arn
   principal     = "events.amazonaws.com"
-
-  source_arn = aws_cloudwatch_event_rule.security_detection.arn
+  source_arn    = aws_cloudwatch_event_rule.security_detection_rule.arn
 }
--- terraform/modules/cloudwatch/outputs.tf
+++ terraform/modules/cloudwatch/outputs.tf
@@ -1,9 +1,9 @@
 output "event_rule_name" {
-  description = "Name of the CloudWatch Event rule"
-  value       = aws_cloudwatch_event_rule.security_detection.name
+  description = "Name of the EventBridge rule"
+  value       = aws_cloudwatch_event_rule.security_detection_rule.name
 }
 
 output "event_rule_arn" {
-  description = "ARN of the CloudWatch Event rule"
-  value       = aws_cloudwatch_event_rule.security_detection.arn
+  description = "ARN of the EventBridge rule"
+  value       = aws_cloudwatch_event_rule.security_detection_rule.arn
 }
