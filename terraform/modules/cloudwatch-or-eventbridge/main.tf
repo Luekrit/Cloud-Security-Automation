4f530614d3146fa6eedb04ac0ca370779b197b91
Luekrit Kongkamon <luekrit.k@gmail.com>
2026-03-13 00:46:58 +1100

    Update cloudwatch/Eventbridge

---
+++ terraform/modules/cloudwatch-or-eventbridge/main.tf
@@ -0,0 +1,33 @@
+locals {
+  rule_name = "${var.project_name}-${var.environment}-${var.rule_name_suffix}"
+}
+
+resource "aws_cloudwatch_event_rule" "this" {
+  name        = local.rule_name
+  description = var.rule_description
+
+  event_pattern = jsonencode({
+    source      = ["aws.iam"]
+    detail-type = ["AWS API Call via CloudTrail"]
+    detail = {
+      eventSource = ["iam.amazonaws.com"]
+      eventName   = var.event_names
+    }
+  })
+
+  tags = var.tags
+}
+
+resource "aws_cloudwatch_event_target" "lambda" {
+  rule      = aws_cloudwatch_event_rule.this.name
+  target_id = "InvokeRemediationLambda"
+  arn       = var.lambda_function_arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this.arn
+}
