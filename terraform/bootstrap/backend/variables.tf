9f49022734b1801299a9c8892b06c50390e01fb6
Luekrit Kongkamon <luekrit.k@gmail.com>
2026-03-17 01:22:32 +1100

    Add main.tf, variables.tf, providers.tf,outputs.tf. and terraform.tfvars and code for each of them

---
+++ terraform/bootstrap/backend/variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region for Terraform backend resources"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile used for backend bootstrap"
+  type        = string
+  default     = "terraform"
+}
+
+variable "state_bucket_name" {
+  description = "Name of the S3 bucket used for Terraform remote state"
+  type        = string
+}
+
+variable "lock_table_name" {
+  description = "Name of the DynamoDB table used for Terraform state locking"
+  type        = string
+  default     = "terraform-state-lock"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging"
+  type        = string
+  default     = "cloud-security-automation"
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "shared"
+}
