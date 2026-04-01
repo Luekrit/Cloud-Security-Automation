c3a829871ddcc5c0b254ddd2afc48b2ac16214af
Luekrit Kongkamon <luekrit.k@gmail.com>
2026-04-01 22:43:56 +1100

    modified iam/main.tf and add guadrail confuration in remediate,py

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -21,3 +21,22 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy" "lambda_remediation_policy" {
+  name = "${var.project_name}-${var.environment}-lambda-remediation-policy"
+  role = aws_iam_role.lambda_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowDetachUserPolicy"
+        Effect = "Allow"
+        Action = [
+          "iam:DetachUserPolicy"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
